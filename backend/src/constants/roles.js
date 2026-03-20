165be916622b78d59bdcb852e031521a9284efbf
Luong Viet Tien <luongviettienns@gmail.com>
2026-03-20 15:45:25 +0700

    Cap nhat frontend

--- backend/src/constants/roles.js
+++
@@ -1,5 +0,0 @@
-export const ROLES = {
-  ADMIN: 'admin',
-  LECTURER: 'lecturer',
-  STUDENT: 'student',
-}
--- frontend/src/pages/LoginPage.jsx
+++
@@ -1,7 +0,0 @@
-import AuthLoginPage from '../features/auth/AuthLoginPage'
-
-function LoginPage() {
-  return <AuthLoginPage />
-}
-
-export default LoginPage
